7c9d7c40d7e54200b2168555059144669e7c9327
SSolucao <suporte@supersolucao.com.br>
2026-01-08 07:52:50 -0300

    ajustes visualizacao guia browser

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "SSolução POS",
+  title: "Sistema de gestão e PDV",
   description: "Sistema de gestão e PDV",
 }
 
